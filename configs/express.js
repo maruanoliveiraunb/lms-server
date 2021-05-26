c2ca9d25c7fd78df77caf47b11c25851455b192c
Maruan Oliveira <maruanoliveira@gmail.com>
2021-05-26 14:21:40 -0300

    altera variavel de busca da porta

--- configs/express.js
+++ configs/express.js
@@ -6,7 +6,7 @@ const { authJwt, authHeaders } = require("../middlewares");
 
 module.exports = () => {
     const app = express()
-    const port = process.env.SERVER_PORT || 3000
+    const port = process.env.PORT || 3000
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
